dd38c6aadfe49debeac41b62b21c90d8436d5223
wwj <1941796841@qq.com>
2020-07-10 16:57:30 +0800

    第一次提交

---
+++ daily/src/main/java/demo/daily/test/CC.java
@@ -0,0 +1,14 @@
+
+package demo.daily.test;
+
+/**
+ * 
+ * @author wei
+ * @Description: TODO
+ * @date 2020-7-10
+ */
+public class CC {
+	
+	
+
+}
---
+++ daily/src/main/java/demo/daily/test/Main.java
@@ -0,0 +1,12 @@
+package demo.daily.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a;
+		int a;
+		int b;
+		
+	}
+		
+}
