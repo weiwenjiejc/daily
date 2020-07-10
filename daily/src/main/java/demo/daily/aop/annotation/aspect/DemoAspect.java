61166312dc4fab596660f659dc46174ed5a4f1c5
wwj <1941796841@qq.com>
2020-07-10 17:26:11 +0800

    更新

---
+++ daily/src/main/java/demo/daily/aop/annotation/aspect/DemoAspect.java
@@ -0,0 +1,12 @@
+package demo.daily.aop.annotation.aspect;
+
+/**
+ * 
+ * @author wei
+ * @Description: 切面类Demo，切面是通知和切点的结合，通知和切点共同定义了切面的全部内容。
+ *   			因为通知定义的是切面的"要做什么"和"在何时做"，而切点定义的是切面的"在何地做"。
+ * @date 2020-7-10
+ */
+public class DemoAspect {
+
+}
--- daily/src/main/java/demo/daily/test/Main.java
+++ daily/src/main/java/demo/daily/test/Main.java
@@ -1,12 +1,13 @@
 package demo.daily.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int a;
-		int a;
-		int b;
-		
+		String[] configLocations = new String[] {"classpath:spring-aop-annotation.xml"};
+		ApplicationContext app = new ClassPathXmlApplicationContext();
 	}
 		
 }
