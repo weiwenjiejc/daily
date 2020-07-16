8410764266e8fa6e6429799fecfb74fcd6c0deda
wwj <1941796841@qq.com>
2020-07-16 15:54:53 +0800

    ss

---
+++ demo/src/main/java/org/wwj/demo/aop/aspect/DemoAspect.java
@@ -0,0 +1,18 @@
+package demo.daily.aop.annotation.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author wei
+ * @Description: 切面类Demo，切面是通知和切点的结合，通知和切点共同定义了切面的全部内容。
+ *   			因为通知定义的是切面的"要做什么"和"在何时做"，而切点定义的是切面的"在何地做"。
+ * @date 2020-7-10
+ */
+
+@Aspect
+@Component
+public class DemoAspect {
+
+}
---
+++ demo/src/main/java/org/wwj/demo/aop/controller/DemoController.java
@@ -0,0 +1,15 @@
+package demo.daily.aop.annotation.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+	
+	
+	public void demo() {
+		System.out.println("demo");
+	}
+
+
+
+}
