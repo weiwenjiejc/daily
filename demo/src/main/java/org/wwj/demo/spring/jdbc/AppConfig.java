70b5c7e715c59c8339ee5c5e1901347ebfedaa5f
wwj <1941796841@qq.com>
2020-07-16 16:20:39 +0800

    ss

--- demo/src/main/java/org/wwj/demo/spring/jdbc/AppConfig.java
+++ demo/src/main/java/org/wwj/demo/spring/jdbc/AppConfig.java
@@ -2,8 +2,15 @@ package org.wwj.demo.spring.jdbc;
 
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @ImportResource
+ * 导入一个传统的xml配置文件
+ */
 
 @Configurable
+@ImportResource(locations = {"classpath:spring-jdbc.xml"})
 public class AppConfig {
 
 }
