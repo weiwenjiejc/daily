dc1cef96235da931cab7278010a965d51b5fd0ff
wwj <1941796841@qq.com>
2020-07-16 16:21:14 +0800

    ss

--- demo/src/main/java/org/wwj/demo/spring/jdbc/SpringBootApp.java
+++ demo/src/main/java/org/wwj/demo/spring/jdbc/SpringBootApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
 @SpringBootApplication
-public class Main {
+public class SpringBootApp {
 
     public static void main(String[] args) {
 
